78b2836da3bf02f1a6bd81e2882bdd3b11ec9c38
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-02-08 17:42:19 +0200

    data.title created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courseTitle = 'angular-course';
+
+  data= {
+    title: 'Angular-course'
+  };
+
 }
